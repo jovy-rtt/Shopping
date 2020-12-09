8fdcdd7611d01c97130db2574c5437c918a900c9
Villin尼 <2436981550@qq.com>
2020-12-09 09:43:03 +0800

    新建了项目，买家商家订单区域，区域中的控制器及布局页

---
+++ Shopping/Shopping/Areas/Buyers/Controllers/BuyersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping.Areas.Buyers.Controllers
+{
+    //买家控制类
+    public class BuyersController : Controller
+    {
+        // GET: Buyers/Buyers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shopping/Shopping/Areas/Merchants/Controllers/MerchantsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping.Areas.Merchants.Controllers
+{
+    //商家控制类
+    public class MerchantsController : Controller
+    {
+        // GET: Merchants/Merchants
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
