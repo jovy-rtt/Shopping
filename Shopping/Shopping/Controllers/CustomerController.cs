4d1d930c6d3116d1ae9be77c90ffdb5ac4c26a06
DogBrother <dogbrother@yeah.net>
2020-12-22 15:30:52 +0800

    实现用户的注册、登录、忘记密码、登出

--- Shopping/Shopping/Controllers/CustomerController.cs
+++ Shopping/Shopping/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shopping.CS_Init;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,8 @@ namespace Shopping.Controllers
         // GET: Customer
         public ActionResult Index(string id)
         {
+            PeachData_init context = new PeachData_init();
+            context.InitializeDatabase(new Models.PeachMd());
             if (id == null)
                 id = "Index";
             if (Request.IsAjaxRequest())
